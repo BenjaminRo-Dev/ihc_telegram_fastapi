4484b17a046137c72c5c78026a07524b9b991e05
Benjamin Romero <programador.ben@gmail.com>
2025-12-02 00:44:28 -0400

    feat: :sparkles: agregado campos created_at y updated_at a los modelos de la bd

--- app/models/modelos.py
+++ app/models/modelos.py
@@ -8,6 +8,8 @@ class Delivery(SQLModel, table=True):
     password: str
     ubicacion: str
     disponible: bool = True
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), sa_column_kwargs={"onupdate": lambda: datetime.now(timezone.utc)})
 
     pedidos: list["Pedido"] = Relationship(back_populates="delivery")
 
@@ -15,6 +17,8 @@ class Delivery(SQLModel, table=True):
 class Categoria(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     nombre: str
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), sa_column_kwargs={"onupdate": lambda: datetime.now(timezone.utc)})
 
     platos: list["Plato"] = Relationship(back_populates="categoria")
 
@@ -24,6 +28,8 @@ class Plato(SQLModel, table=True):
     nombre: str
     precio_venta: float
     url_imagen: str
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), sa_column_kwargs={"onupdate": lambda: datetime.now(timezone.utc)})
 
     categoria_id: int = Field(foreign_key="categoria.id")
     categoria: Categoria = Relationship(back_populates="platos")
@@ -39,6 +45,8 @@ class Pedido(SQLModel, table=True):
     precio_delivery: float
     chat_id: str
     nombre_usuario: str
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), sa_column_kwargs={"onupdate": lambda: datetime.now(timezone.utc)})
 
     delivery_id: int | None = Field(default=None, foreign_key="delivery.id")
 
@@ -52,6 +60,8 @@ class Detalle(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     cantidad: int
     observacion: str
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), sa_column_kwargs={"onupdate": lambda: datetime.now(timezone.utc)})
 
     pedido_id: int = Field(foreign_key="pedido.id")
     plato_id: int = Field(foreign_key="plato.id")
@@ -65,3 +75,5 @@ class Configuracion(SQLModel, table=True):
     ubicacion_restaurante: str
     precio_km: float
     precio_base: float
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), sa_column_kwargs={"onupdate": lambda: datetime.now(timezone.utc)})
